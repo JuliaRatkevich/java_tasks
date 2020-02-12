9d525c606d1798e48456f0ca0aa81333082fc382
Julia <juliakuntsevich@gmail.com>
2020-02-12 21:36:29 +0100

    add OurTreeSet

---
+++ 20_02_06_set_task1/src/com/julia/collection/OurTreeSetTest.java
@@ -0,0 +1,7 @@
+package com.julia.collection;
+
+import static org.junit.Assert.*;
+
+public class OurTreeSetTest {
+
+}
--- 20_02_06_set_task1/src/com/julia/collection/TreeSet.java
+++
@@ -1,6 +0,0 @@
-package com.julia.collection;
-
-public class TreeSet extends OurHashSet {
-
-
-}
