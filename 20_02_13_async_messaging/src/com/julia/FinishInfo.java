42b92d6762ce74b84f3c8bb1c24daa7910eafc55
Julia <juliakuntsevich@gmail.com>
2020-02-14 16:06:58 +0100

    no message

---
+++ 20_02_13_async_messaging/src/com/julia/FinishInfo.java
@@ -0,0 +1,15 @@
+package com.julia;
+import java.util.List;
+
+
+public class FinishInfo  {
+
+    int tarakanId;
+    long finishTime;
+
+
+    public FinishInfo(int tarakanId, long finishTime) {
+        this.tarakanId = tarakanId;
+        this.finishTime = finishTime;
+    }
+}
