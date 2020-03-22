63dcb60e7ed4720c3810c665eec97ec3853a941f
Julia <juliakuntsevich@gmail.com>
2020-03-22 20:31:18 +0100

    no message

--- 20_03_21_balancer/src/com/julia/Balancer.java
+++ 20_03_21_balancer/src/com/julia/Balancer.java
@@ -1,5 +1,4 @@
 package com.julia;
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -9,35 +8,35 @@ public class Balancer implements Runnable {
     private DatagramSocket socket;
     private byte[] buf = new byte[256];
 
-    private ArrayList<ServerDescription> serverDescriptions;
+    private List<ServerDescription> serverDescriptions;
     private static Integer position = 0;
 
     public Balancer(ArrayList<ServerDescription> serverDescriptions) throws Exception {
-        this.serverDescriptions = serverDescriptions;
+        this.serverDescriptions = Collections.synchronizedList(serverDescriptions);
     }
 
     public Integer getTarget() {
         Integer target;
-        synchronized (position) {
-            if (position > serverDescriptions.size() - 1) {
-                position = 0;
-            }
+//        synchronized (position) {
+//            if (position > serverDescriptions.size() - 1) {
+//                position = 0;
+//            }
 //            target = servers.get(position).getPort();
-            position++;
+//            position++;
 
-            ServerDescription serverDescription = serverDescriptions.stream()
-                    .min(Comparator.comparing(ServerDescription::getWorkload))
-                    .get();
+        ServerDescription serverDescription = serverDescriptions.stream()
+                .min(Comparator.comparing(ServerDescription::getWorkload))
+                .get();
 
-            System.out.println("----------------------------------------------------");
-            serverDescriptions.stream().forEach(s ->
-                        System.out.println(s.getId() + ":" + s.getWorkload())
-                     );
-            System.out.println("Picked: " + serverDescription.getId() + ":" + serverDescription.getWorkload());
-            System.out.println("----------------------------------------------------");
+        System.out.println("----------------------------------------------------");
+        serverDescriptions.stream().forEach(s ->
+                System.out.println(s.getId() + ":" + s.getWorkload())
+        );
+        System.out.println("Picked: " + serverDescription.getId() + ":" + serverDescription.getWorkload());
+        System.out.println("----------------------------------------------------");
 //            System.out.println(server.getId() + ":" + server.getWorkload());
-            target = serverDescription.getPort();
-        }
+        target = serverDescription.getPort();
+        increaseWorklodFor(serverDescription.getId());
         return target;
     }
 
@@ -65,17 +64,17 @@ public class Balancer implements Runnable {
     }
 
     private void handleServerMessage(String message, Integer port) {
-        if (message.contains("_submitted_")) {
-            String serverId = message.split("#")[1];
-            ServerDescription serverDescriptionWithId = serverDescriptions.stream()
-                    .filter(serverDescription -> serverDescription.getId().equals(serverId))
-                    .findFirst()
-                    .orElse(null);
-            if (serverDescriptionWithId != null) {
-                Integer workload = serverDescriptionWithId.getWorkload();
-                serverDescriptionWithId.setWorkload(workload + 1);
-            }
-        }
+//        if (message.contains("_submitted_")) {
+//            String serverId = message.split("#")[1];
+//            ServerDescription serverDescriptionWithId = serverDescriptions.stream()
+//                    .filter(serverDescription -> serverDescription.getId().equals(serverId))
+//                    .findFirst()
+//                    .orElse(null);
+//            if (serverDescriptionWithId != null) {
+//                Integer workload = serverDescriptionWithId.getWorkload();
+//                serverDescriptionWithId.setWorkload(workload + 1);
+//            }
+//        }
 
         if (message.contains("_handled_")) {
             String serverId = message.split("#")[1];
@@ -87,6 +86,17 @@ public class Balancer implements Runnable {
                 Integer workload = serverDescriptionWithId.getWorkload();
                 serverDescriptionWithId.setWorkload(workload - 1);
             }
+        }
+    }
+
+    private void increaseWorklodFor(String serverId) {
+        ServerDescription serverDescriptionWithId = serverDescriptions.stream()
+                .filter(serverDescription -> serverDescription.getId().equals(serverId))
+                .findFirst()
+                .orElse(null);
+        if (serverDescriptionWithId != null) {
+            Integer workload = serverDescriptionWithId.getWorkload();
+            serverDescriptionWithId.setWorkload(workload + 1);
         }
     }
 }
